d1f3034262a2d68f8d1098749eb93866e109e593
Robert Pech <robert.pech@uni-tuebingen.de>
2016-02-25 10:55:48 +0100

    port can be specified

--- scripts/eva2server.sh
+++ scripts/eva2server.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-java -Djava.util.logging.config.file=logging.properties -jar eva2server.jar
+if [[ $1 ]]; then
+	java -Djava.util.logging.config.file=logging.properties -jar eva2server.jar --port $1
+else
+	java -Djava.util.logging.config.file=logging.properties -jar eva2server.jar
+fi
